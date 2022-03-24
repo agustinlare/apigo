c49b4d4b69759b2cce195cc098e17d505165f744
Agustin Lavarello <43295081+agustinlare@users.noreply.github.com>
2022-03-23 23:53:07 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
-	"time"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-
+func contador(endpoint string) {
 	flag := os.Getenv("COUNTER_HIT_GOLANG")
 	count, err := strconv.Atoi(flag)
 
@@ -22,28 +19,43 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 	newValue := strconv.Itoa(count + 1)
 	os.Setenv("COUNTER_HIT_GOLANG", newValue)
-	fmt.Println("Endpoint Hit: homePage", newValue)
+	fmt.Println("Endpoint Hit:", endpoint, newValue)
+}
 
-	if strings.Contains(newValue, "100") {
-		time.Sleep(5 * time.Second)
-		fmt.Println("Endpoint Hit by 5: homePage", newValue)
-	} else if strings.Contains(newValue, "40") {
-		time.Sleep(2 * time.Second)
-		fmt.Println("Endpoint Hit by 2: homePage", newValue)
-	} else {
-		time.Sleep(2 * time.Second)
-		fmt.Println("Endpoint Hit by 0: homePage", newValue)
-	}
+func resp_health(w http.ResponseWriter, r *http.Request) {
+	mapD := map[string]string{"response": "ok"}
+	mapB, _ := json.Marshal(mapD)
 
+	fmt.Fprintf(w, string(mapB))
+	contador("health")
+}
+
+func resp_unhealthy(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+
+	mapD := map[string]string{"response": "cof...cof"}
+	mapB, _ := json.Marshal(mapD)
+
+	fmt.Fprintf(w, string(mapB))
+	contador("unhealthy")
+}
+
+func resp_pong(w http.ResponseWriter, r *http.Request) {
+	mapD := map[string]string{"ping": "pong"}
+	mapB, _ := json.Marshal(mapD)
+
+	fmt.Fprintf(w, string(mapB))
+	contador("ping-pong")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/health", resp_health)
+	http.HandleFunc("/unhealthy", resp_unhealthy)
+	http.HandleFunc("/ping", resp_pong)
 	fmt.Println("Server Up")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
 
 func main() {
-
 	handleRequests()
 }
